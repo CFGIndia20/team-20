1dc3d49650bbc999c9e3b49e9fcfa7439b84445e
77CaptainClaw77 <kumar1235813@gmail.com>
2020-07-26 01:53:52 +0530

    fixed model image upload path for auth endpoints

--- backend/umeed/users/models.py
+++ backend/umeed/users/models.py
@@ -5,12 +5,12 @@ from PIL import Image
 class UserProfile(models.Model):
     user_acc=models.OneToOneField(User,on_delete=models.CASCADE,null=False)
     area = models.CharField(null=False,max_length=100)
-    image = models.ImageField(default='default.jpg', upload_to="",null=True)
+    image = models.ImageField(default='default.jpg', upload_to="user_images",null=True)
     skills= models.TextField(null=False)
 
 class Manager(models.Model):
     phn_number = models.CharField(max_length=12,null=True)
-    image = models.ImageField(default='default.jpg', upload_to="",null=True)
+    image = models.ImageField(default='default.jpg', upload_to="manager_images",null=True)
     area = models.CharField(null=False,max_length=100)
     user_acc=models.OneToOneField(User,on_delete=models.CASCADE,null=False)
     unset=models.BooleanField(default=True) #check if profile is filled
